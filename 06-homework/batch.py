b3d2cf9d634b208fcaadc3ae36761ac1b8ae5d6c
rshiva <nadarshiva.r@gmail.com>
2025-06-27 18:21:54 -0700

    06 answer for 2,3 and 4

--- 06-homework/batch.py
+++ 06-homework/batch.py
@@ -6,9 +6,7 @@ import pickle
 import pandas as pd
 
 
-def read_data(filename, categorical):
-    df = pd.read_parquet(filename)
-    
+def prepare_data(df, categorical):
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
 
@@ -18,9 +16,36 @@ def read_data(filename, categorical):
     
     return df
 
+import os
+
+def get_input_path(year, month):
+    default_input_pattern = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+
+def get_output_path(year, month):
+    default_output_pattern = f'taxi_type=yellow_year={year:04d}_month={month:02d}.parquet'
+    output_pattern = os.getenv('OUTPUT_FILE_PATTERN', default_output_pattern)
+    return output_pattern.format(year=year, month=month)
+
+def read_data(filename, categorical):
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+    if S3_ENDPOINT_URL:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+        df = pd.read_parquet(filename, storage_options=options)
+    else:
+        df = pd.read_parquet(filename)
+    
+    return df
+
 def main(year, month):
-    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f'taxi_type=yellow_year={year:04d}_month={month:02d}.parquet'
+    input_file = get_input_path(year, month)
+    output_file = get_output_path(year, month)
     
     with open('model.bin', 'rb') as f_in:
         dv, lr = pickle.load(f_in)
@@ -29,6 +54,7 @@ def main(year, month):
     categorical = ['PULocationID', 'DOLocationID']
     
     df = read_data(input_file, categorical)
+    df = prepare_data(df, categorical)
     df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
     
     
@@ -45,7 +71,16 @@ def main(year, month):
     df_result['predicted_duration'] = y_pred
     
     
-    df_result.to_parquet(output_file, engine='pyarrow', index=False)
+    S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
+    if S3_ENDPOINT_URL:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': S3_ENDPOINT_URL
+            }
+        }
+        df_result.to_parquet(output_file, engine='pyarrow', index=False, storage_options=options)
+    else:
+        df_result.to_parquet(output_file, engine='pyarrow', index=False)
 
 
 if __name__ == '__main__':
